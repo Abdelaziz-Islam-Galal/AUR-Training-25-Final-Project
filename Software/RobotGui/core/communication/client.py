be8453b585ff304a6de46b187f27a8a1a8142a1c
Abdelaziz Islam Galal <abdelaziz.islam.galal@gmail.com>
2025-10-15 17:05:40 +0300

    applied threading for publishing

--- Software/RobotGui/core/communication/client.py
+++ Software/RobotGui/core/communication/client.py
@@ -3,6 +3,9 @@ from paho.mqtt.enums import CallbackAPIVersion
 
 from RobotGui.core.communication.subscribe.coordinates import Coordinates
 
+import time
+from threading import Thread
+
 class Mqtt():
     def __init__(self, coordinates_slot, address = 'localhost', port = 1883):
         self.publishing_setup() #for all publishing to all topics
@@ -15,4 +18,21 @@ class Mqtt():
         self.pub.user_data_set(self.unacked_publish)
         self.pub.connect("localhost", 1883)
         self.pub.loop_start()
+
+    def publish_msg(self, mqtt_client, topic, *args):
+        if len(args) == 1:
+            message = str(args[0])
+        else:
+            message = ",".join(str(arg) for arg in args)
+        
+        self._pub_thread = Thread(target=self._publishing, args=[mqtt_client, topic, message], daemon=True)
+        self._pub_thread.start()
+        
+    def _publishing(self, mqtt_client, topic, message):    
+        msg = mqtt_client.pub.publish(topic, message)
+        if mqtt_client.unacked_publish is not None:
+            mqtt_client.unacked_publish.add(msg.mid)
+        while len(mqtt_client.unacked_publish):
+            time.sleep(0.1)
+        msg.wait_for_publish()
 
