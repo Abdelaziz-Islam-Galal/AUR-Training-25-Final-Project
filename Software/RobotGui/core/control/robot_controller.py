a2a4855d793c89373fcc7e54150bad68903fce80
mohamed abdelkarim <mohamedelshbiny2005@gmail.com>
2025-10-17 22:18:02 +0300

    controller pygame code added

--- Software/RobotGui/core/control/robot_controller.py
+++ Software/RobotGui/core/control/robot_controller.py
@@ -7,7 +7,7 @@ class RobotController():
     def setMode(self,new_mode:Mode):
         print(f'switching to {new_mode.name}')
         self.mode=new_mode
-    def choose_mode(self,cmd):  #receives command from GUI window 
+    def choose_mode(self,cmd:list):  #receives command from GUI window 
         if self.mode==Mode.Manual:
             self.manual_control(cmd)
         elif self.mode==Mode.Full_Auto:
