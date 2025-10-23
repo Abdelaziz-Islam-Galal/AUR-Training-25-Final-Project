b7d6790b0b26bc6c4c6ea04fc36c87c81eef2d27
yasso2006 <yassin.awad2006@gmail.com>
2025-10-23 20:08:34 +0300

    Update aotounmous.py

--- Software/RobotGui/core/auto/aotounmous.py
+++ Software/RobotGui/core/auto/aotounmous.py
@@ -2,13 +2,13 @@ import cv2
 from RobotGui.core.cv.QR_scanner import qr_scanner
 import numpy as np
 from time import time
-from RobotGui.gui.minimap import Minimap
+from RobotGui.core.communication.subscribe.Subscribers_methods import SubscribersMethods
 from RobotGui.core.cv.cv import Camera
 
 def qr_auto():
 
-    coords = Minimap
-    x,y,theta = Minimap
+    coords = SubscribersMethods()
+    x,y,theta = coords.coordinates()
     frame = Camera() 
     while y > 2:
         img = frame.frame()
